2f81dbff8ee93d79e6e8d5646bf5878c20f77bf9
Tomas Acuña <tomas10_agustin@hotmail.com>
2025-09-20 21:32:52 -0300

    Mensaje de tu cambio

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { CartProvider } from './context/CartContext'   // <-- IMPORTANTE
+import { CartProvider } from './context/CartContext'   
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider>     {/* <-- envolvemos App con el provider */}
+    <CartProvider>     
       <App />
     </CartProvider>
   </React.StrictMode>,
